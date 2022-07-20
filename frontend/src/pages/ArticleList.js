47dc9a7fb88fa2998e03a8921e66fef93e154bc1
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-19 22:12:52 -0700

    Blog Pages

---
+++ frontend/src/pages/ArticleList.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const ArticleList = () => {
+  return (
+    <div>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>  Articles </h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Duis sint est occaecat ut exercitation mollit mollit ea ullamco. 
+            Excepteur nisi in eiusmod qui tempor id tempor id tempor dolore 
+            ullamco veniam elit. Sit sint officia esse laborum amet tempor 
+            ullamco magna ex.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Aliqua laboris magna sint irure exercitation reprehenderit consequat ut. Elit aliqua adipisicing 
+            sint tempor nostrud proident non eiusmod ut nulla incididunt consectetur. Velit nulla id aute 
+            eiusmod aliqua. Sint sunt consectetur et duis proident pariatur nulla aute in deserunt ullamco duis.
+             Ipsum mollit proident ullamco anim ea reprehenderit. Ut ullamco nulla id fugiat adipisicing ut pariatur.
+              Commodo minim qui laboris aliqua eiusmod incididunt eiusmod reprehenderit voluptate dolor sit magna occaecat.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Aliqua laboris magna sint irure exercitation reprehenderit consequat ut. Elit aliqua adipisicing 
+            sint tempor nostrud proident non eiusmod ut nulla incididunt consectetur. Velit nulla id aute 
+            eiusmod aliqua. Sint sunt consectetur et duis proident pariatur nulla aute in deserunt ullamco duis.
+             Ipsum mollit proident ullamco anim ea reprehenderit. Ut ullamco nulla id fugiat adipisicing ut pariatur.
+              Commodo minim qui laboris aliqua eiusmod incididunt eiusmod reprehenderit voluptate dolor sit magna occaecat.
+
+              Aliqua laboris magna sint irure exercitation reprehenderit consequat ut. Elit aliqua adipisicing 
+            sint tempor nostrud proident non eiusmod ut nulla incididunt consectetur. Velit nulla id aute 
+            eiusmod aliqua. Sint sunt consectetur et duis proident pariatur nulla aute in deserunt ullamco duis.
+             Ipsum mollit proident ullamco anim ea reprehenderit. Ut ullamco nulla id fugiat adipisicing ut pariatur.
+              Commodo minim qui laboris aliqua eiusmod incididunt eiusmod reprehenderit voluptate dolor sit magna occaecat.
+        </p>
+    </div>
+  )
+}
+
+export default ArticleList
