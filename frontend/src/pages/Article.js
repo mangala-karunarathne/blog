ce41fcd6a0714e42a2ac08358bbf256d37b92af2
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-23 08:30:31 -0700

    Article List

--- frontend/src/pages/Article.js
+++ frontend/src/pages/Article.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import k from './article-content'
+import articleContent from './article-content' 
+// Here articleContent is not a component but it just a name to call that entire article content
 
 const Article = () => {
   const {name} = useParams(); 
-  const article = k.find((article)=>article.name ===name);
+  const article = articleContent.find((article)=>article.name ===name);
   if (!article) return <h1> Article does not exist </h1>
   return (
     <div>
        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
          {article.title}
        </h1> 
-       {article.content.map((paragraph, index)=>(
+       {articleContent.map((paragraph, index)=>(
         <p className='mx-auto leading-relaxed text-base mb-4' key={index}>
           {paragraph}
+          console.log(paragraph);
         </p>    
             ))}
     </div>
--- frontend/src/pages/ArticleList.js
+++ frontend/src/pages/ArticleList.js
@@ -1,37 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import articleContent from "./article-content"
 import Navbar from '../components/Navbar'
 
 const ArticleList = () => {
   return (
     <div>
-      <Navbar/>
-        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>  Articles </h1>
-        <p className='mx-auto leading-relaxed text-base mb-4'>
-            Duis sint est occaecat ut exercitation mollit mollit ea ullamco. 
-            Excepteur nisi in eiusmod qui tempor id tempor id tempor dolore 
-            ullamco veniam elit. Sit sint officia esse laborum amet tempor 
-            ullamco magna ex.
-        </p>
-        <p className='mx-auto leading-relaxed text-base mb-4'>
-            Aliqua laboris magna sint irure exercitation reprehenderit consequat ut. Elit aliqua adipisicing 
-            sint tempor nostrud proident non eiusmod ut nulla incididunt consectetur. Velit nulla id aute 
-            eiusmod aliqua. Sint sunt consectetur et duis proident pariatur nulla aute in deserunt ullamco duis.
-             Ipsum mollit proident ullamco anim ea reprehenderit. Ut ullamco nulla id fugiat adipisicing ut pariatur.
-              Commodo minim qui laboris aliqua eiusmod incididunt eiusmod reprehenderit voluptate dolor sit magna occaecat.
-        </p>
-        <p className='mx-auto leading-relaxed text-base mb-4'>
-            Aliqua laboris magna sint irure exercitation reprehenderit consequat ut. Elit aliqua adipisicing 
-            sint tempor nostrud proident non eiusmod ut nulla incididunt consectetur. Velit nulla id aute 
-            eiusmod aliqua. Sint sunt consectetur et duis proident pariatur nulla aute in deserunt ullamco duis.
-             Ipsum mollit proident ullamco anim ea reprehenderit. Ut ullamco nulla id fugiat adipisicing ut pariatur.
-              Commodo minim qui laboris aliqua eiusmod incididunt eiusmod reprehenderit voluptate dolor sit magna occaecat.
-
-              Aliqua laboris magna sint irure exercitation reprehenderit consequat ut. Elit aliqua adipisicing 
-            sint tempor nostrud proident non eiusmod ut nulla incididunt consectetur. Velit nulla id aute 
-            eiusmod aliqua. Sint sunt consectetur et duis proident pariatur nulla aute in deserunt ullamco duis.
-             Ipsum mollit proident ullamco anim ea reprehenderit. Ut ullamco nulla id fugiat adipisicing ut pariatur.
-              Commodo minim qui laboris aliqua eiusmod incididunt eiusmod reprehenderit voluptate dolor sit magna occaecat.
-        </p>
+     <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>
+        Articles
+      </h1>
+      <div className='container py-4 mx-auto'>
+        <div className='flex flex-wrap -m-4'>
+          {articleContent.map((article, index)=>(
+            <div key={index} className="p-4 md:w-1/2"> 
+              <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg
+              overflow-hidden'>
+                <Link to={`/article/${article.name}`}>
+                  <img 
+                      className="lg:h-48 md:h-36 w-full object-cover object-center" 
+                      src={article.thumbnail} 
+                      alt='blog'
+                  />
+                </Link>
+              </div>            
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
