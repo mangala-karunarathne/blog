66f52b508fb4102ab48fe846ea37ee3d6a2c2a86
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-23 13:50:19 -0700

    Article List Update

--- frontend/src/pages/Article.js
+++ frontend/src/pages/Article.js
@@ -14,9 +14,14 @@ const Article = () => {
        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
          {article.title}
        </h1> 
-        <p className='mx-auto leading-relaxed text-base mb-4'>
-          {article.content}
-        </p>
+        
+          {article.content.map((paragraph, index) => (
+            <p className='mx-auto leading-relaxed text-base mb-4' key={index}>
+            {paragraph}
+            </p>
+          ))}
+
+        
     </div>
     </>
     
