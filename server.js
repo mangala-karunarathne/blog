33c2f8f0d90e39b6d493e803977a038630a0bf76
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-24 14:40:27 -0700

    Rout Parameters in Express

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express ();
 const PORT = process.env.PORT || 8000;
 
+// Initialize middleware
+// we used to install body parser but now it's a built in middleware
+// Function of express. It parses incoming JSONpayload
+// app.use(express.json({extended:false}));
+app.use(express.json({extended: false}));
+
 // Test Rout
-app.get('/',(req,res)=>res.send("Hello Mangala !!!"));
+app.get("/", (req,res)=>res.send("Hello Aruna !!!"));
+
+
+app.post("/", (req,res)=>res.send(`Hello ${req.body.name}`)); 
+app.get("/hello/:name", (req,res) => res.send(`Hello ${req.params.name}`))
 
 app.listen(PORT, ()=> console.log(`Server is running at port ${PORT}`));
