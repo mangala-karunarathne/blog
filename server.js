6461e7c981e4ca6c6cc7d585cf5ad740206ac49a
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-28 01:09:41 -0700

    Adding MongoDB to Express

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 const express = require("express");
 const app = express ();
+const { MongoClient } = require("mongodb");
 const PORT = process.env.PORT || 8000;
 
-const articlesinfo = {
-    "learn-react":{
-        comments: [],
-    },
-    "learn-node":{
-        comments: [],
-    },
-    "my-thoughts-on-learning-reacts":{
-        comments: [],
-    },
-};
 
 // Initialize middleware
 // we used to install body parser but now it's a built in middleware
@@ -24,6 +14,20 @@ app.use(express.json({extended: false}));
 // app.get("/", (req,res)=>res.send("Hello Aruna !!!"));
 // app.post("/", (req,res)=>res.send(`Hello ${req.body.name} `));
 // app.get("/hello/:name", (req.res)=>res.send(`Hello ${req.params.name}`))
+
+app.get('/api/articles/:name', async (req,res)=>{
+    try {
+        const articleName = req.params.name;
+        const client = await MongoClient.connect('mongodb://localhost:27017')
+        const db = client.db("mernblog")
+        const articlesinfo = db.collection('articles').findOne({name: articleName})
+        res.status(200).jason(articlesinfo)
+        client.close();
+    } catch (error) {
+        res.status(500).jason({message: "Error connecting to database", error})
+    }
+    
+});
 app.post('/api/articles/:name/add-comments', (req,res)=>{
   const {username, text} = req.body;
   const articleName = req.params.name;
